892b1b644016a3996b302a0759e14514968897a0
nimakhanjari <nimakhanjari1@gmail.com>
2024-12-10 12:36:16 +0330

    dorost shod koskesh

--- parallel/Voice.cpp
+++ parallel/Voice.cpp
@@ -65,4 +65,84 @@ void Voice::band_pass_filter(float down, float up) {
         }
     }
 }
+void Voice::readWavFile(const string& inputFile, vector<float>& data, SF_INFO& fileInfo) {
+    SNDFILE* inFile = sf_open(inputFile.c_str(), SFM_READ, &fileInfo);
+    if (!inFile) {
+        std::cerr << "Error opening input file: " << sf_strerror(NULL) << std::endl;
+        exit(1);
+    }
+
+    data.resize(fileInfo.frames * fileInfo.channels);
+    size_t numThreads = 4; // Number of threads
+    pthread_t threads[numThreads];
+    ThreadData threadData[numThreads];
+
+    size_t chunkSize = data.size() / numThreads;
+
+    for (size_t i = 0; i < numThreads; ++i) {
+        threadData[i] = {this, i * chunkSize, (i == numThreads - 1) ? data.size() : (i + 1) * chunkSize};
+        pthread_create(&threads[i], nullptr, readChunk, &threadData[i]);
+    }
+
+    for (size_t i = 0; i < numThreads; ++i) {
+        pthread_join(threads[i], nullptr);
+    }
+
+    sf_close(inFile);
+    std::cout << "Successfully read " << fileInfo.frames << " frames from " << inputFile << std::endl;
+}
+
+void* Voice::readChunk(void* arg) {
+    ThreadData* threadData = static_cast<ThreadData*>(arg);
+    Voice* voice = threadData->voice;
+    size_t start = threadData->start;
+    size_t end = threadData->end;
+
+    sf_count_t numFrames = sf_readf_float(voice->data.data() + start, end - start);
+    if (numFrames != end - start) {
+        std::cerr << "Error reading frames from file in chunk." << std::endl;
+        exit(1);
+    }
+    return nullptr;
+}
+void Voice::writeWavFile(const string& outputFile, SF_INFO& fileInfo) {
+    SNDFILE* outFile = sf_open(outputFile.c_str(), SFM_WRITE, &fileInfo);
+    if (!outFile) {
+        std::cerr << "Error opening output file: " << sf_strerror(NULL) << std::endl;
+        exit(1);
+    }
+
+    size_t numThreads = 4; // Number of threads
+    pthread_t threads[numThreads];
+    ThreadData threadData[numThreads];
+
+    size_t chunkSize = data.size() / numThreads;
+
+    for (size_t i = 0; i < numThreads; ++i) {
+        threadData[i] = {this, i * chunkSize, (i == numThreads - 1) ? data.size() : (i + 1) * chunkSize};
+        pthread_create(&threads[i], nullptr, writeChunk, &threadData[i]);
+    }
+
+    for (size_t i = 0; i < numThreads; ++i) {
+        pthread_join(threads[i], nullptr);
+    }
+
+    sf_close(outFile);
+    std::cout << "Successfully wrote " << fileInfo.frames << " frames to " << outputFile << std::endl;
+}
+
+void* Voice::writeChunk(void* arg) {
+    ThreadData* threadData = static_cast<ThreadData*>(arg);
+    Voice* voice = threadData->voice;
+    size_t start = threadData->start;
+    size_t end = threadData->end;
+
+    sf_count_t numFrames = sf_writef_float(voice->data.data() + start, end - start);
+    if (numFrames != end - start) {
+        std::cerr << "Error writing frames to file in chunk." << std::endl;
+        exit(1);
+    }
+    return nullptr;
+}
+
 
--- parallel/Voice.hpp
+++ parallel/Voice.hpp
@@ -15,13 +15,21 @@ class Voice
 private:
     string input_file;
     vector<float> data;
+    SF_INFO fileInfo;
+
+    struct ThreadData {
+        Voice* voice;
+        size_t start;
+        size_t end;
+    };
+
+    static void* readChunk(void* arg);
+    static void* writeChunk(void* arg);
 
 public:
     Voice(const string& inputFile, vector<float>& data, SF_INFO& fileInfo);
     ~Voice();
     void readWavFile(const string& inputFile, vector<float>& data, SF_INFO& fileInfo);
     void writeWavFile(const string& outputFile, SF_INFO& fileInfo);
-    void band_pass_filter(float down, float up);
-    void notch_filter(float removed_frequency);
 };
 
