c9d107595260549c288eaee48deb71ae9a6ff4b1
behradbina <behradbina@gmail.com>
2024-12-10 11:46:38 +0330

    initializing project

---
+++ serial/Voice.hpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <sndfile.h>
+#include <string>
+#include <cstring>
+using namespace std;
+
+class Voice
+{
+private:
+    string input_file;
+    vector<float> data;
+
+public:
+    Voice(const string& inputFile, vector<float>& data, SF_INFO& fileInfo);
+    ~Voice();
+    void readWavFile(const string& inputFile, vector<float>& data, SF_INFO& fileInfo);
+    void writeWavFile(const string& outputFile, const vector<float>& data,  SF_INFO& fileInfo);
+    void band_pass_filter(float down, float up);
+    void notch_filter(float removed_frequency);
+};
+
